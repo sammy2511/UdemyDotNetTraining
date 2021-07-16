750caf9c36b3ab5b5df2ed55a9b7be6054070447
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2021-07-16 21:04:34 +0530

    initial commit

---
+++ donNetTraining/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace donNetTraining { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            stack.Clear();
+            stack.Push(9);
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ donNetTraining/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace donNetTraining
+{
+    class Stack
+    {
+        private List<Object> _stackList;
+        private int _top;
+        public Stack()
+        {
+            _stackList = new List<object>();
+            _top = -1;
+        }
+
+        public void Push(Object obj)
+        {
+            if(obj == null)
+            {
+                throw new InvalidOperationException();
+            }
+            _stackList.Add(obj);
+            _top += 1;
+
+
+        }
+
+        public Object Pop()
+        {
+            if(_top == -1)
+            {
+                throw new InvalidOperationException();
+            }
+            var obj = _stackList[_top];
+            _top -= 1;
+            return obj;
+
+        }
+
+        public void Clear()
+        {
+            _top = -1;
+            _stackList.Clear();
+        }
+
+
+    }
+}
+
